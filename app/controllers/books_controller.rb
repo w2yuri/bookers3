89a68452886ed00cc0ea255deb917f06d65839f4
“YuriShozen” <155799910+w2yuri@users.noreply.github.com>
2024-02-17 10:20:17 +0000

    削除OK

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,10 @@
 class BooksController < ApplicationController
   def show
+    @book = Book.find(params[:id])
   end
   
   def create
-    @book = Book.new(bookparams)
+    @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
     redirect_to books_path
@@ -14,10 +15,15 @@ class BooksController < ApplicationController
     @books = Book.all
   end
   
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end 
+    
   private
 
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
-end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :books, only: [:index, :show, :create]
+  resources :books, only: [:index, :show, :create, :destroy]
   devise_for :users
   root to: "homes#top", as: :top
   get 'homes/about' => 'homes#about', as: :about
