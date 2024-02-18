9730517c286142e89918abb7ff469b912e54f256
“YuriShozen” <155799910+w2yuri@users.noreply.github.com>
2024-02-18 05:35:20 +0000

    投稿不備修正完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,31 +2,31 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path(@book.id)
   end
 
   def index
     @book = Book.new
     @books = Book.all
   end
-  
+
   def edit
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
-  end 
-    
+  end
+
   private
 
   def book_params
     params.require(:book).permit(:title, :body)
   end
-end 
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,17 +8,21 @@ class UsersController < ApplicationController
   end
 
   def index
+    @book = Book.new
   end
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
-  end 
-  
+  end
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :text)
-  end 
-end 
+  end
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
