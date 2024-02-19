6f727e3ad783fd42a9e0908b8d3259004d9f3758
“YuriShozen” <155799910+w2yuri@users.noreply.github.com>
2024-02-19 10:40:05 +0000

    更新２

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def create
@@ -13,13 +14,15 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+    @user = current_user
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def destroy
-    book = Book.find(params[:id])
+    @book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :books, only: [:index, :show, :create, :edit, :destroy, :update]
   resources :users, only: [:index, :show, :edit, :create, :update]
-  devise_for :users
-  root to: "homes#top", as: :top
+  root to: "homes#top"
   get 'homes/about' => 'homes#about', as: :about
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
