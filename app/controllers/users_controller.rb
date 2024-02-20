586acc02d08936c19af2b979fda5b9a8272e181e
“YuriShozen” <155799910+w2yuri@users.noreply.github.com>
2024-02-20 09:41:48 +0000

    更新3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,13 @@ class UsersController < ApplicationController
     @books = @user.books
   end
 
+ def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+ end
+
   def edit
     @user = User.find(params[:id])
   end
@@ -29,4 +36,4 @@ class UsersController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-end
+ end 
