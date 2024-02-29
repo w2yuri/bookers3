d95f335fcef3792b4127bd92ed095497f40c4f82
“YuriShozen” <155799910+w2yuri@users.noreply.github.com>
2024-02-29 07:56:24 +0000

    フォロー機能作成中

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_one_attached :image
 
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -11,6 +10,12 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
 
    has_one_attached :profile_image
+
+   has_many :active_relatiomships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+   has_many :followings, through: :active_relationships, source: :followed
+   has_many :followers, through: :passive_relationships, source: :follower
+
 
    validates :name, uniqueness: true
    validates :name, length: { minimum: 2, maximum: 20 }
@@ -24,5 +29,17 @@ class User < ApplicationRecord
   end
   profile_image.variant(resize_to_limit: [width, height]).processed
   end
-end
 
+  def follow(user)
+    active_relationships.create(followed_id: user.id)
+  end
+
+  def unfollow(user)
+    active_relationships.find_by(followed_id: user.id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
+
+end
